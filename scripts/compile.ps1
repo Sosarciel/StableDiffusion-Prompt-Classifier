36b821c5155229efc9de192300368230a56fbf30
zwa73 <ssfen2419@126.com>
2026-07-05 12:04:55 +0800

    fx

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -1,5 +1,12 @@
-﻿Write-Output 开始删除原dist
-Remove-Item -Recurse -Force dist
+﻿# compile v1.0.0
+if (Test-Path dist) {
+    Write-Output "开始删除原dist"
+    Remove-Item -Recurse -Force dist
+}
 Write-Output 开始编译
 tsc -p tsconfig.compile.json
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "tsc 编译失败"
+    exit $LASTEXITCODE
+}
 tsc-alias -p tsconfig.compile.json
